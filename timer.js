e78e04e230f5cbd6f23aed78c04462a5fe2989c4
njang <neo.w.jang@gmail.com>
2018-01-11 22:14:17 -0600

    Add basic button functions

--- timer.js
+++ timer.js
@@ -1,27 +1,78 @@
 
 const secInMin = 60;
-let minute;
-let second;
-$('.timer').text('0:00');
+const increment = 100; // incremend in milliseconds. Reduce it for faster countdown during development.
 
+let sessionDuration = 9;
+let breakDuration = 5;
 
+(() => {
+	// let displayElement = $('<input>', {id: 'display', placeholder: 0, disabled: true})
+	// $('.calculator').append(displayElement);
+	// $('.calculator').append($('<br>'));
 
+	// let buttons = [["&plusmn;", "&radic;", "&percnt;", "&div;"], [7, 8, 9, "&times;"], [4, 5, 6, "&ndash;"], [1, 2, 3, "&plus;"], [".", 0, "&equals;", "C"]];
+	// for (let i = 0; i < buttons.length; i++) {
+	// 	for (let j = 0; j < buttons[i].length; j++) {
+	// 		let buttonValue = (Number.isInteger(buttons[i][j]) ? buttons[i][j] : buttons[i][j].replace(/[^A-Za-z.]/g, ''));
+	// 		let buttonElement = $('<button>', {class: 'bg-dark text-white font-weight-bold', value: buttonValue })
+	// 		buttonElement.html(buttons[i][j]);
+	// 		$('.calculator').append(buttonElement);	
+	// 	}
+	// 	$('.calculator').append($('<br>'));
+	// }
+	// $('[value="C"]').attr('class', 'bg-danger text-white font-weight-bold');
+
+	$('#session').html(sessionDuration + ":00");
+	$('#break').html(breakDuration + ":00");
+
+})();
+
+// $('#session').html('25:00');
+// $('#break').html('05:00');
+
+// main function that runs the timer for the requested duration
 const timer = (duration) => {
 	duration *= secInMin;
 	const countDown = setInterval(() => {
-		second = duration % secInMin;
-		minute = (duration - second)/secInMin;
+		let second = duration % secInMin;
+		let minute = (duration - second)/secInMin;
 
-		if (second.toString().split('').length == 1) {
-			second = "0" + second.toString();
-			// console.log(second + 'sec');
+		let secondString = second.toString().split('');
+		if (secondString.length == 1) {
+			secondString.unshift("0");
 		}
+		secondString = secondString.join('');
 
 		if (duration <= 0) {
 			clearInterval(countDown);
 		}
-		$('.timer').text(`${minute}:${second}`);
+		$('#session').html(`${minute}:${secondString}`);
 		duration--;	
-	}, 100);
+	}, increment);
 }
 
+$(document).ready("#button").click(function(event) {
+	let entered = event.target.id;
+	switch (entered) {
+		case "sessionMinus":
+			sessionDuration -= 1;
+			break;
+		case "session":
+			timer(sessionDuration);
+			break;
+		case "sessionPlus":
+			sessionDuration += 1;
+			break;
+		case "breakMinus":
+			breakDuration -= 1;
+			break;
+		case "break":
+			break;
+		case "breakPlus":
+			breakDuration += 1;
+			break;
+	}
+	$('#session').html(sessionDuration + ":00");
+	$('#break').html(breakDuration + ":00");
+	
+});
